0097b189eb2a0084fb5d86c6b3c5f8b81ca357be
Superman <703353105@qq.com>
2018-06-28 15:53:48 +0800

    1st commit

---
+++ UpLoadVideo/pickerController/VideoPickerController.h
@@ -0,0 +1,136 @@
+//
+//  VideoPickerController.h
+//  UpLoadVideo
+//
+//  Created by Superman on 2018/6/28.
+//  Copyright © 2018年 Superman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AssetsLibrary/AssetsLibrary.h>
+
+@protocol VideoPickerControllerDelegate;
+
+@interface VideoPickerController : UINavigationController
+@property (nonatomic, weak) id <UINavigationControllerDelegate, VideoPickerControllerDelegate> delegate;
+
+@property (nonatomic, strong) ALAssetsFilter *assetsFilter;
+
+@property (nonatomic, copy, readonly) NSArray *indexPathsForSelectedItems;
+
+@property (nonatomic, assign) NSInteger maximumNumberOfSelection;
+@property (nonatomic, assign) NSInteger minimumNumberOfSelection;
+
+@property (nonatomic, strong) NSPredicate *selectionFilter;
+
+@property (nonatomic, assign) BOOL showCancelButton;
+
+@property (nonatomic, assign) BOOL showEmptyGroups;
+
+@property (nonatomic, assign) BOOL isFinishDismissViewController;
+
+@end
+@protocol VideoPickerControllerDelegate <NSObject>
+
+-(void)assetPickerController:(VideoPickerController *)picker didFinishPickingAssets:(NSArray *)assets;
+
+@optional
+
+-(void)assetPickerControllerDidCancel:(VideoPickerController *)picker;
+
+-(void)assetPickerController:(VideoPickerController *)picker didSelectAsset:(ALAsset*)asset;
+
+-(void)assetPickerController:(VideoPickerController *)picker didDeselectAsset:(ALAsset*)asset;
+
+-(void)assetPickerControllerDidMaximum:(VideoPickerController *)picker;
+
+-(void)assetPickerControllerDidMinimum:(VideoPickerController *)picker;
+
+@end
+
+@interface AssetViewController : UITableViewController
+
+@property (nonatomic, strong) ALAssetsGroup *assetsGroup;
+@property (nonatomic, strong) NSMutableArray *indexPathsForSelectedItems;
+
+@end
+
+#pragma mark - VideoTitleView
+
+@interface VideoTitleView : UILabel
+
+@end
+
+#pragma mark - TapAssetView
+
+@protocol TapAssetViewDelegate <NSObject>
+
+-(void)touchSelect:(BOOL)select;
+-(BOOL)shouldTap;
+
+@end
+
+@interface TapAssetView : UIView
+
+@property (nonatomic, assign) BOOL selected;
+@property (nonatomic, assign) BOOL disabled;
+@property (nonatomic, weak) id<TapAssetViewDelegate> delegate;
+
+@end
+
+#pragma mark - AssetView
+
+@protocol AssetViewDelegate <NSObject>
+
+-(BOOL)shouldSelectAsset:(ALAsset*)asset;
+-(void)tapSelectHandle:(BOOL)select asset:(ALAsset*)asset;
+
+@end
+
+@interface AssetView : UIView
+
+- (void)bind:(ALAsset *)asset selectionFilter:(NSPredicate*)selectionFilter isSeleced:(BOOL)isSeleced;
+
+@end
+
+#pragma mark - AssetViewCell
+
+@protocol AssetViewCellDelegate;
+
+@interface AssetViewCell : UITableViewCell
+
+@property(nonatomic,weak)id<AssetViewCellDelegate> delegate;
+
+- (void)bind:(NSArray *)assets selectionFilter:(NSPredicate*)selectionFilter minimumInteritemSpacing:(float)minimumInteritemSpacing minimumLineSpacing:(float)minimumLineSpacing columns:(int)columns assetViewX:(float)assetViewX;
+
+@end
+
+@protocol AssetViewCellDelegate <NSObject>
+
+- (BOOL)shouldSelectAsset:(ALAsset*)asset;
+- (void)didSelectAsset:(ALAsset*)asset;
+- (void)didDeselectAsset:(ALAsset*)asset;
+
+@end
+
+#pragma mark - AssetGroupViewCell
+
+@interface AssetGroupViewCell : UITableViewCell
+
+- (void)bind:(ALAssetsGroup *)assetsGroup;
+
+@end
+
+#pragma mark - AssetGroupViewController
+
+@interface AssetGroupViewController : UITableViewController
+
+
+
+
+
+@end
+
+
+
+@end
---
+++ UpLoadVideo/pickerController/VideoPickerController.m
@@ -0,0 +1,37 @@
+//
+//  VideoPickerController.m
+//  UpLoadVideo
+//
+//  Created by Superman on 2018/6/28.
+//  Copyright © 2018年 Superman. All rights reserved.
+//
+
+#import "VideoPickerController.h"
+
+@interface VideoPickerController ()
+
+@end
+
+@implementation VideoPickerController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
